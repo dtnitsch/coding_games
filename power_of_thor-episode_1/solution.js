d0c9a91ab1498d8f14fe88fd2fdb17f3b299d71d
Daniel Tisza-Nitsch <dtnitsch@gmail.com>
2018-03-27 12:36:13 -0700

    Update solution.js

--- power_of_thor-episode_1/solution.js
+++ power_of_thor-episode_1/solution.js
@@ -11,9 +11,14 @@ var lightY = parseInt(inputs[1]); // the Y position of the light of power
 var initialTX = parseInt(inputs[2]); // Thor's starting X position
 var initialTY = parseInt(inputs[3]); // Thor's starting Y position
 
+// Figure out if X or Y is longer
+// ... the longer one is where we will hmm.
 x = Math.abs(lightX - initialTX);
 y = Math.abs(lightY - initialTY);
 
+// Assuming people take ONE step at a time in the shorter direction
+// ... the longer on will talk as long as they can in a given direction before
+// ... taking a step horizontally
 stepx = 1;
 stepy = 1;
 if(x > y) {
@@ -22,6 +27,7 @@ if(x > y) {
     stepx = Math.floor(y / x);
 }
 
+// Setting the initial location
 x = initialTX;
 y = initialTY;
 
@@ -32,7 +38,12 @@ while (true) {
     // Write an action using print()
     // To debug: printErr('Debug messages...');
     
+    // Blank out the X direction in case we don't need it.
     directionx = '';
+    // If the current location is the SAME as the end result, no more changes to X
+    // ... Otherwise, figure out if we are going left or right, and where we currently are (initalTX)
+    //
+    // Note - Modulus:  Only step diagonally when we need to.
     if(initialTX != lightX && x++ % stepx == 0) {
         directionx = (initialTX < lightX ? 'E' : 'W');
         initialTX += (initialTX <= lightX ? 1 : -1);
